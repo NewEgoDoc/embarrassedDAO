336ac254334e8224cbd3b2def20afe63f74e60a8
Juhan <juhan211@naver.com>
2021-12-07 22:45:19 +0900

    초난감 DAO

---
+++ src/spring/user/dao/UserDao.java
@@ -0,0 +1,52 @@
+package spring.user.dao;
+
+import spring.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://172.30.1.45:5432/postgres";
+        String username = "postgres";
+        String password = "postgres";
+
+        Connection connection = DriverManager.getConnection(url,username,password);
+        PreparedStatement pstmt = connection.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+        pstmt.setString(1,user.getId());
+        pstmt.setString(2,user.getName());
+        pstmt.setString(3,user.getPassword());
+
+        pstmt.executeUpdate();
+
+        pstmt.close();
+        connection.close();
+
+    }
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://172.30.1.45:5432/postgres";
+        String username = "postgres";
+        String password = "postgres";
+
+        Connection connection = DriverManager.getConnection(url,username,password);
+        PreparedStatement pstmt = connection.prepareStatement("select * from users where id = ?");
+        pstmt.setString(1,id);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        pstmt.close();
+        connection.close();
+
+        return user;
+    }
+}
---
+++ src/spring/user/test.java
@@ -0,0 +1,24 @@
+package spring.user;
+
+import spring.user.dao.UserDao;
+import spring.user.domain.User;
+
+import java.sql.SQLException;
+
+public class test {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+        
+        User user = new User();
+        user.setId("white");
+        user.setName("백기선");
+        user.setPassword("married");
+        
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println("user2 name = " + user2.getName());
+    }
+}
